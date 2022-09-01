d608ffded6d1e835b574c0166bae3d1a184f00fe
amolmane7471 <maneamol3233@gmail.com>
2022-09-01 09:49:20 +0530

    [Amol] : added stack and queue

---
+++ src/com/bridgelabz/stack/StackAndQueueMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.stack;
+/*
+ * purpose : implement stack and queue data structre using linked list
+ * @uthor : Amol
+ * since : 2022/08/31
+ */
+public class StackAndQueueMain {
+	public static void main(String[] args) {
+	System.out.println("*** Welcome to stack and queue data structure ***");
+	}
+}
